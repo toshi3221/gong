07fa26a041025ae0b9a6b1919ecbb7de4f79eda9
Toshiharu TAKEMATSU <info@newfish.jp>
2011-06-05 22:59:16 +0900

    Gongアプリ初期プロジェクト作成

---
+++ src/org/example/gong/Gong.java
@@ -0,0 +1,13 @@
+package org.example.gong;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Gong extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
