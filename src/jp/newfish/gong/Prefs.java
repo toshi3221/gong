daffe457b9d4facdbc69febe44cd549b54e56ddc
Toshiharu Takematsu <info@newfish.jp>
2013-01-31 23:51:24 +0900

    Update version 1.3. fix prefs Long.parse error.

--- src/jp/newfish/gong/Prefs.java
+++ src/jp/newfish/gong/Prefs.java
@@ -6,6 +6,10 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 
 public class Prefs extends PreferenceActivity {
+
+	final static String GONG_INTERVAL_DEFAULT = "300";
+	final static String GONG_TIMES_DEFAULT = "1";
+
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -13,12 +17,17 @@ public class Prefs extends PreferenceActivity {
 	}
 	
 	public static Long getGongInterval(final Context context) {
-		return Long.valueOf(PreferenceManager.getDefaultSharedPreferences(context)
-					.getString("gong_interval", "300"));
+		String interval = PreferenceManager.getDefaultSharedPreferences(context)
+				.getString("gong_interval", GONG_INTERVAL_DEFAULT);
+		if (interval == null || interval.length() == 0) { interval = GONG_INTERVAL_DEFAULT; }
+		return Long.valueOf(interval);
 	}
 
 	public static Long getGongTimes(final Context context) {
-		return Long.valueOf(PreferenceManager.getDefaultSharedPreferences(context)
-					.getString("gong_times", "1"));
+		
+		String gongTimes = PreferenceManager.getDefaultSharedPreferences(context)
+				.getString("gong_times", GONG_TIMES_DEFAULT);
+		if (gongTimes == null || gongTimes.length() == 0) { gongTimes = GONG_TIMES_DEFAULT; }
+		return Long.valueOf(gongTimes);
 	}
 }
